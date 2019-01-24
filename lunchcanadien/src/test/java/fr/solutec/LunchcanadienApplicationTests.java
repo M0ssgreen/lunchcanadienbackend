33ce9c88314f82e23c64823b1cf04737eb04ceaf
François <francois.sam@consultants-solutec.fr>
2019-01-24 13:52:14 +0100

    création d'une classe User dans enitites

---
+++ lunchcanadien/src/test/java/fr/solutec/LunchcanadienApplicationTests.java
@@ -0,0 +1,17 @@
+package fr.solutec;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LunchcanadienApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
