f761a0397664cf21d41ef348cc70551c57eca077
gabrielter <gabriel.c.t.terrien@gmail.com>
2019-01-24 15:52:21 +0100

    repository creation gab

---
+++ lunchcanadien/src/main/java/fr/solutec/dao/UserRepository.java
@@ -0,0 +1,11 @@
+package fr.solutec.dao;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import fr.solutec.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
