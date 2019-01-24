9cdef27fbdf45aa7f3b0a9deefe5806922d34cd1
François <francois.sam@consultants-solutec.fr>
2019-01-24 13:55:05 +0100

    creation des var pour User

--- lunchcanadien/src/main/java/fr/solutec/entities/User.java
+++ lunchcanadien/src/main/java/fr/solutec/entities/User.java
@@ -1,5 +1,86 @@
 package fr.solutec.entities;
 
-public class User {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable {
+	
+	@Id @GeneratedValue
+	private Long id;
+	private String nom;
+	private String prenom;
+	private String mail;
+	private String mdp;
+	
+	public User( String nom, String mail) {
+		super();
+		this.nom = nom;
+		this.mail = mail;
+	}
+	
+	public String getPrenom() {
+		return prenom;
+	}
+
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+
+	public String getMdp() {
+		return mdp;
+	}
+
+
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+	public String getMail() {
+		return mail;
+	}
+
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	
+	
+	
+	
 }
