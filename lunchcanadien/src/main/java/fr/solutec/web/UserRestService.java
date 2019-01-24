6b4851d06f333a04e42969d28de0f3167303e2b0
Anais <anais.poujouly@hotmail.fr>
2019-01-24 16:52:42 +0100

    Modif UserRestService

--- lunchcanadien/src/main/java/fr/solutec/web/UserRestService.java
+++ lunchcanadien/src/main/java/fr/solutec/web/UserRestService.java
@@ -1,5 +1,20 @@
 package fr.solutec.web;
 
-public class UserRestService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import fr.solutec.dao.UserRepository;
+import fr.solutec.entities.User;
+
+public class UserRestService {
+	@Autowired
+	private UserRepository userRepo;
+	
+	@RequestMapping(value="/users", method = RequestMethod.GET)
+	public List<User> getUsers(){
+		return userRepo.findAll();
+	}
 }
