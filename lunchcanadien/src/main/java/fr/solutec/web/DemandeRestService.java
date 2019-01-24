22322ab4e38d38bccadcc8311b9c423603a01250
François <francois.sam@consultants-solutec.fr>
2019-01-24 16:30:38 +0100

    ajout method get pour les demandes

--- lunchcanadien/src/main/java/fr/solutec/web/DemandeRestService.java
+++ lunchcanadien/src/main/java/fr/solutec/web/DemandeRestService.java
@@ -1,5 +1,25 @@
 package fr.solutec.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.solutec.dao.DemandeRepository;
+import fr.solutec.entities.Demande;
+
+@RestController
 public class DemandeRestService {
+	
+	@Autowired
+	private DemandeRepository demandeRepo;
+	
+	@RequestMapping(value="/demandes", method=RequestMethod.GET)
+	public List<Demande> getDemande(){
+		return demandeRepo.findAll();
+	}
+	
 
 }
