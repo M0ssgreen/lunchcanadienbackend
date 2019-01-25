750d1fd3c1428ad6c12de921e536c88fd18c3e55
François <francois.sam@consultants-solutec.fr>
2019-01-25 16:44:53 +0100

    modif method post demande

--- lunchcanadien/src/main/java/fr/solutec/web/DemandeRestService.java
+++ lunchcanadien/src/main/java/fr/solutec/web/DemandeRestService.java
@@ -33,7 +33,7 @@ public class DemandeRestService {
 	
 
 	@RequestMapping(value="/demandes", method=RequestMethod.POST)
-	public Demande saveDemande( Demande d) {
+	public Demande saveDemande(Demande d) {
 		return demandeRepo.save(d);
 	}
 	
