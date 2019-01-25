0a0cb8f2218a830c5c2877659ed8c7140ad118c9
François <francois.sam@consultants-solutec.fr>
2019-01-25 09:37:38 +0100

    ajout d'un crossorigin pour que les données puissent passer dans chrome et ajout d'une methode pour récupérer une demande

--- lunchcanadien/src/main/java/fr/solutec/web/UserRestService.java
+++ lunchcanadien/src/main/java/fr/solutec/web/UserRestService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import fr.solutec.dao.UserRepository;
 import fr.solutec.entities.User;
 
 @RestController
+@CrossOrigin("*")
 public class UserRestService {
 	
 	@Autowired
