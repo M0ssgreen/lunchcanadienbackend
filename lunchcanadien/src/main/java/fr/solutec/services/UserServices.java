03ea4932696a9a47e51987e6606c3ee414dcc330
François <francois.sam@consultants-solutec.fr>
2019-01-28 14:42:34 +0100

    correction annotation et ajout d'annotation dans l'application

--- lunchcanadien/src/main/java/fr/solutec/services/UserServices.java
+++ lunchcanadien/src/main/java/fr/solutec/services/UserServices.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +13,7 @@ import fr.solutec.entities.Event;
 import fr.solutec.entities.User;
 import fr.solutec.web.*;
 
-@RestController
-@CrossOrigin("*")
+@Service
 public class UserServices {
 	
 	@Autowired
