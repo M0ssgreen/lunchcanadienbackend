712531b9b14db230f4a379ba619bd71b8449dc54
gabrielter <gabriel.c.t.terrien@gmail.com>
2019-01-28 10:39:23 +0100

    maj entites gab 28/01

--- lunchcanadien/src/main/java/fr/solutec/entities/Demande.java
+++ lunchcanadien/src/main/java/fr/solutec/entities/Demande.java
@@ -12,70 +12,24 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 public class Demande {
-	@Id @GeneratedValue
-	private Long idDemande;
 	@ManyToOne(cascade=CascadeType.MERGE)
 	private Event event;
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm")
-	private Date dateDebut;
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm")
-	private Date dateFin;
-	private boolean status;
 	@ManyToOne(cascade=CascadeType.MERGE)
 	private User user;
-	
-	
-	
-	public Demande(Date dateDebut, Date dateFin) {
-		super();
-		this.dateDebut = dateDebut;
-		this.dateFin = dateFin;
-	}
-
-	public Demande(Long idDemande, Event idEvent, Date dateDebut, Date dateFin, boolean status, User user) {
-		super();
-		this.idDemande = idDemande;
-		this.event = idEvent;
-		this.dateDebut = dateDebut;
-		this.dateFin = dateFin;
-		this.user = user;
-	}
-	
-	public boolean isStatus() {
-		return status;
-	}
-
-	public void setStatus(boolean status) {
-		this.status = status;
-	}
-
 	public Demande() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Long getIdDemande() {
-		return idDemande;
+	public Demande(Event event, User user) {
+		super();
+		this.event = event;
+		this.user = user;
 	}
-	public void setIdDemande(Long idDemande) {
-		this.idDemande = idDemande;
-	}
-	public Event getIdEvent() {
+	public Event getEvent() {
 		return event;
 	}
-	public void setIdEvent(Event event) {
+	public void setEvent(Event event) {
 		this.event = event;
-	}
-	public Date getDateDebut() {
-		return dateDebut;
-	}
-	public void setDateDebut(Date dateDebut) {
-		this.dateDebut = dateDebut;
-	}
-	public Date getDateFin() {
-		return dateFin;
-	}
-	public void setDateFin(Date dateFin) {
-		this.dateFin = dateFin;
 	}
 	public User getUser() {
 		return user;
@@ -83,6 +37,9 @@ public class Demande {
 	public void setUser(User user) {
 		this.user = user;
 	}
+	
+
+
 	
 	
 
