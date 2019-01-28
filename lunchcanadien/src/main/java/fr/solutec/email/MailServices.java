0e579915f0cce039113a1000ad15f4abaac50590
Anais <anais.poujouly@hotmail.fr>
2019-01-28 16:53:29 +0100

    Création MailServices + Liens dans EventRestService

--- lunchcanadien/src/main/java/fr/solutec/email/MailServices.java
+++ lunchcanadien/src/main/java/fr/solutec/email/MailServices.java
@@ -1,6 +1,7 @@
 package fr.solutec.email;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Properties;
 
 import javax.mail.Message;
@@ -12,16 +13,21 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import fr.solutec.entities.User;
 
 @Service
 public class MailServices {
 	
-	protected String mail = "usertest.lunch@gmail.com";
-	protected String password = "lunchcanadien";
 	
-	public void envoyerMail(String contenuMail, String mailDestinataire, String sujetMail) throws IOException{
-		
-
+	
+	 private void sendMail(String destinataire, String sujet, String contenu)  {
+		final String mail = "usertest.lunch@gmail.com";
+		final String password = "lunchcanadien";
+		//String contenuMail = "OLALA, gab est le meilleur!!!!";
+		//String sujetMail = "IMPORTANT";
+		//String mailDestinataire = "anais.poujouly@hotmail.fr";
 		Properties props = new Properties();
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.starttls.enable", "true");
@@ -39,9 +45,9 @@ public class MailServices {
 
 			Message message = new MimeMessage(session);
 			message.setFrom(new InternetAddress(mail));
-			message.addRecipients(Message.RecipientType.TO, InternetAddress.parse(mailDestinataire));
-			message.setSubject(sujetMail);
-			message.setText(contenuMail);
+			message.addRecipients(Message.RecipientType.TO, InternetAddress.parse(destinataire));
+			message.setSubject(sujet);
+			message.setText(contenu);
 
 			Transport.send(message);
 
@@ -50,6 +56,31 @@ public class MailServices {
 		} catch (MessagingException e) {
 			throw new RuntimeException(e);
 		}
-		
-	}
+	 }
+
+	 public void envMail(User u ) {
+			String sujet = "Création d'un LunchCanadien";
+			String Newligne=System.getProperty("line.separator");
+			String contenu = "Bonjour " + u.getPrenom() + "," + Newligne + Newligne + "Vous venez de poster vos disponibilités. "+ Newligne + "Vous recevrez un nouvel email dès que 2 collègues auront un créneau horaire correspondant au votre. " + Newligne + Newligne + "Patience ... et bonne journée !";
+			sendMail(u.getMail(), sujet, contenu);
+		}
+	 
+	 public void envMailGroupe(List<User> users) {
+		 String sujet = "[LunchCanadien] Match !";
+		 String Newligne=System.getProperty("line.separator");
+		 for (User u:users) {
+			String contenu = "Bonjour " + u.getPrenom() + "," + Newligne + Newligne + "Des collègues sont disponibles en même temps que vous. " + Newligne + "Contactez-les ! Leurs coordonnées sont disponibles dans votre Lunch." + Newligne + Newligne + "Bonne journée !";
+			sendMail(u.getMail(), sujet, contenu);
+		 }
+	 }
+	 
+	 public void envMailRappel(List<User> users) {
+			String sujet = "[LunchCanadien] Rappel";
+			String Newligne=System.getProperty("line.separator");
+			for (User u:users) {
+				String contenu = "Bonjour " + u.getPrenom() + "," + Newligne + Newligne + "Noubliez pas votre lunch prévu ce midi... Vos collègues vous attendent ! " + Newligne + Newligne + "Bon appétit.";
+				sendMail(u.getMail(), sujet, contenu);
+			}
+			
+		}
 }
--- lunchcanadien/src/main/java/fr/solutec/web/EventRestService.java
+++ lunchcanadien/src/main/java/fr/solutec/web/EventRestService.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import fr.solutec.dao.EventRepository;
 import fr.solutec.web.DemandeRestService;
 import fr.solutec.dao.UserRepository;
+import fr.solutec.email.MailServices;
 import fr.solutec.entities.Demande;
 import fr.solutec.entities.Event;
 import fr.solutec.entities.User;
@@ -24,6 +25,9 @@ import fr.solutec.services.DemandeServices;
 public class EventRestService {
 	@Autowired
 	private EventRepository eventRepo;
+	
+	@Autowired
+	private MailServices ms;
 	
 	@RequestMapping(value="/events", method=RequestMethod.GET)
 	public List<Event> getEvents(){
@@ -40,6 +44,22 @@ public class EventRestService {
 	public void saveEvent(@RequestBody Event e, User u){
 		DemandeServices.matchEvent(u.getMail(),e.getDate(),u.getPrenom(), u.getNom(), u.getEntreprise());
 		
+	}
+	
+	@RequestMapping(value="/mailDispo", method=RequestMethod.POST)
+	public void envMail1(@RequestBody User u) {
+		ms.envMail(u);
+	}
+	
+	@RequestMapping(value="/mailMatch", method=RequestMethod.POST)
+	public void envMailGroupe1(@RequestBody List<User> users) {
+		ms.envMailGroupe(users);
+		
+	}
+	
+	@RequestMapping(value="/mailRappel", method=RequestMethod.POST)
+	public void envMailRappel1(@RequestBody List<User> users) {
+		ms.envMailRappel(users);
 	}
 	
 	
