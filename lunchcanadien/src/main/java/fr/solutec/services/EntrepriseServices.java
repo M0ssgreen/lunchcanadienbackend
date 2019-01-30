90637b4725c014325b573f93a769b87644458479
Anais <anais.poujouly@hotmail.fr>
2019-01-30 16:53:14 +0100

    Gestion entreprises

---
+++ lunchcanadien/src/main/java/fr/solutec/services/EntrepriseServices.java
@@ -0,0 +1,20 @@
+package fr.solutec.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import fr.solutec.dao.EntrepriseRepository;
+import fr.solutec.entities.Entreprise;
+
+@Service
+public class EntrepriseServices {
+	
+	
+}
